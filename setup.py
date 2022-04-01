0f2f3ece5a57a5c6abd04cee0b19f7102abb71e9
dchahuan <d.chahuan@uc.cl>
2022-04-01 12:04:09 +0200

    hope is your last resort

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matrixCICD',
-    version='0.3',
+    version='0.4',
     license='MIT',
     author="Diego Chahuan and Preston Chao",
     author_email='docl@ug.kth.se',
